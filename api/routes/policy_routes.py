e6611eb518557f6b7b3dcc12eaf7660af06e464d
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-12-16 16:44:56 +0530

    update routes to use DB

--- api/routes/policy_routes.py
+++ api/routes/policy_routes.py
@@ -1,59 +1,59 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 import uuid
-from api.models import Policy
-from api.schemas import PolicyInput, PolicyPutInput, PoliciesResponse
-from api.database import policies
+from sqlalchemy.orm import Session
+
+from api.schemas import PolicyInput, PolicyPutInput, PoliciesResponse, PolicyResponse
+from api.database import get_db
+from api.models import PolicyDB
 router = APIRouter()
 
 
 
 # get all policies
 
-@router.get("/policies", response_model= PoliciesResponse)
-def get_all_policies()->PoliciesResponse:
-    return {"policies": policies}
+@router.get("/policies",  response_model= PoliciesResponse)
+def get_all_policies(db:Session=Depends(get_db))->PoliciesResponse:
+    policies =  db.query(PolicyDB).all()
+    return {"policies":policies}
 
 #get a policy by it's id
 
-@router.get("/policies/{policy_id}", response_model=Policy)
-def get_specific_policy(policy_id:uuid.UUID):
-    for policy in policies:
-        if policy.PolicyID == policy_id:
-            return policy
-    raise HTTPException(status_code=404, detail=f"Policy with id {policy_id} not found")
+@router.get("/policies/{policy_id}", response_model=PolicyResponse)
+def get_specific_policy(policy_id:uuid.UUID, db:Session=  Depends(get_db)):
+    policy= db.query(PolicyDB).filter(PolicyDB.PolicyID == policy_id).first()
+    if policy is None:
+        raise HTTPException(status_code=404, detail=f"Policy with id {policy_id} not found.")
+    return policy
 
 #post a new policy
 
 @router.post("/policies")
-def add_policy(policy: PolicyInput):
-    new_policy= Policy(
-        PolicyID= uuid.uuid1(),
-        **policy.model_dump()
-    )
-    policies.append(new_policy)
-    return {"message": "Policy added successfully"}
+def add_policy(policy: PolicyInput, db:Session= Depends(get_db)):
+    new_policy = PolicyDB(**policy.model_dump())
+    db.add(new_policy)
+    db.commit()
+    db.refresh(new_policy)
+    return {"message": "Policy added successfully", "policy_id": new_policy.PolicyID}
 
 #Update an existing policy
 
 @router.put("/policies/{policy_id}")
-def change_existing_policy(policy_id: uuid.UUID, policy: PolicyPutInput):
-    for idx, existing_policy in enumerate(policies):
-        if existing_policy.PolicyID == policy_id:
-            updated_data = policy.dict(exclude_unset=True)
-            
-            updated_policy = existing_policy.copy(update=updated_data)
-            
-            policies[idx] = updated_policy
-            return {"message": "Policy updated successfully"}
-    
-    raise HTTPException(status_code=404, detail=f"Policy with id {policy_id} not found.")
-
+def change_existing_policy(policy_id: uuid.UUID, policy: PolicyPutInput, db:Session= Depends(get_db)):
+    policy_to_change = db.query(PolicyDB).filter(PolicyDB.PolicyID == policy_id).first()
+    if policy_to_change is None:
+        raise HTTPException(status_code=404, detail=f"Policy with id {policy_id} not found.")
+    for key, value in policy.model_dump(exclude_unset=True).items():
+        setattr(policy_to_change, key, value)
+    db.commit()
+    db.refresh(policy_to_change)
+    return {"message": "Policy updated successfully"}
 
 #delete a policy by it's id
 @router.delete("/policies/{policy_id}")
-def delete_policy(policy_id: uuid.UUID):
-    for policy in policies:
-        if policy.PolicyID == policy_id:
-            policies.remove(policy)
-            return {"message": "Item successfully deleted"}
-    raise HTTPException(status_code=404, detail=f"Policy with id {policy_id} not found.")
+def delete_policy(policy_id: uuid.UUID, db:Session= Depends(get_db)):
+    policy_to_delete= db.query(PolicyDB).filter(PolicyDB.PolicyID == policy_id).first()
+    if policy_to_delete is None:
+        raise HTTPException(status_code=404, detail=f"Policy with id {policy_id} not found.")
+    db.delete(policy_to_delete)
+    db.commit()
+    return {"message": "Policy deleted successfully"}
