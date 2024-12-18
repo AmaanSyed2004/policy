b863d8aa5957cceea361cf66429e8ba0ef2e9b24
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-12-18 14:53:21 +0530

    binding fix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from api.routes.policy_routes import router as policy_router
+from api.database import Base, engine
+from api.models import PolicyDB
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(policy_router)
