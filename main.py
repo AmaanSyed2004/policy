748781eae6d1f887ae3278fc0a54024aed3ebac4
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-12-26 15:31:28 +0530

    add topic creation

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from api.routes.policy_routes import router as policy_router
 from api.database import Base, engine
 from api.models import PolicyDB
+from api.kafka.events import create_topics
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
+create_topics()
 app.include_router(policy_router)
